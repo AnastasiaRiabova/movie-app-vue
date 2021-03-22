2dc771a53bc6ba96e06656650eb91d744508f3b2
AnastasiaRiabova <riabova321@gmail.com>
2021-03-22 15:44:36 +0100

    changes in index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,9 @@ import loader from './modules/loader'
 import modalWindow from './modules/modalWindow'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: { movies, loader, modalWindow }
 })
